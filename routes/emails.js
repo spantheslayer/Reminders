753c38661ed2e3c749f6787af6761b52b0abc611
supriyo <supriyosarkar282@gmail.com>
2021-07-17 00:54:05 +0530

    added scheduled mail

---
+++ routes/emails.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const router = express.Router();
+const nodemailer = require("nodemailer");
+// const cron = require("node-cron");
+const schedule = require("node-schedule");
+
+const HTTPError = require("../errorMessage");
+// const userRoute = require("../routes/users");
+const config = require("../config/default.json");
+const email = require("../model/email");
+
+const smtpConfig = {
+  host: config.aws_ses.host,
+  port: config.aws_ses.port,
+  auth: {
+    user: config.aws_ses.smtp_user,
+    pass: config.aws_ses.smtp_password,
+  },
+};
+
+const transporter = nodemailer.createTransport(smtpConfig);
+const sendMail = (mailOptions) => {
+  new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return reject(error);
+      } else {
+        console.log("email reminder send");
+        return resolve(info);
+      }
+    });
+  });
+};
+
+router.route("/").post(async (req, res) => {
+  try {
+    const reminder_email = req.body.reminder_email;
+    const reminder_name = req.body.reminder_name;
+    const description = req.body.description;
+    const scheduled_time = req.body.scheduled_time;
+
+    if (!reminder_email) throw new HTTPError(400, "Reminder email missing");
+    if (!reminder_name) throw new HTTPError(400, "Reminder name missing");
+    if (!description) throw new HTTPError(400, "description missing");
+    if (!scheduled_time) throw new HTTPError(400, "date missing");
+
+    const newReminder = new email({
+      reminder_email,
+      reminder_name,
+      description,
+      scheduled_time,
+    });
+
+    newReminder.save(() => {
+      res.status(200).json({ status: "ok" });
+      const date = new Date(scheduled_time);
+      schedule.scheduleJob(date, () => {
+        const mailOptions = {
+          from: `"${config.aws_ses.from_name}" <${config.aws_ses.from_email}>`,
+          to: newReminder.reminder_email,
+          subject: newReminder.reminder_name,
+          text: description,
+        };
+        sendMail(mailOptions);
+      });
+    });
+  } catch (err) {
+    return res.status(err.statusCode || 400).json({ status: "error", message: err.message });
+  }
+});
+
+module.exports = router;
