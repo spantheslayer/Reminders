6290d236c5a3148ccd626c46ea1c03a7436d8f8d
supriyo <supriyosarkar282@gmail.com>
2021-07-17 00:53:28 +0530

    made it simple

---
+++ model/email.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const emailSchema = new Schema({
+  reminder_name: String,
+  description: String,
+  scheduled_time: Date,
+  reminder_email: String,
+});
+
+module.exports = mongoose.model("email", emailSchema);
